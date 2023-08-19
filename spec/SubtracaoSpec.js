0ff7a5da062e12574a3fef7a67fa688216ccd867
Fernando <fsagaz2008@gmail.com>
2023-08-18 21:00:17 -0300

    teste

--- spec/SubtracaoSpec.js
+++ spec/SubtracaoSpec.js
@@ -9,7 +9,7 @@ describe("SubTração de Valores", function () {
     describe("Subtração", function () {
 
         it("Espera-se que a subtração dos valores seja 15", function () {
-            expect(resultado.subtracao()).toEqual(15);
+            expect(resultado.subtracao()).toEqual(10);
         });
     });
 });
